14342ab842357cbbf03d2e2c20696f6d3107ac09
Meg <ai285063@gmail.com>
2021-06-09 19:41:34 +0800

    feat: modify register feature

--- routes/modules/users.js
+++ routes/modules/users.js
@@ -2,7 +2,7 @@ const express = require('express')
 const router = express.Router()
 
 // const passport = require('passport')
-// const bcrypt = require('bcryptjs')
+const bcrypt = require('bcryptjs')
 
 const db = require('../../models')
 const User = db.User
@@ -21,8 +21,27 @@ router.get('/register', (req, res) => {
 
 router.post('/register', (req, res) => {
   const { name, email, password, confirmPassword } = req.body
-  User.create({ name, email, password })
-    .then(user => res.redirect('/'))
+  User.findOne({ where: { email } }).then(user => {
+    if (user) {
+      console.log('User already exists')
+      return res.render('register', {
+        name,
+        email,
+        password,
+        confirmPassword
+      })
+    }
+    return bcrypt
+      .genSalt(10)
+      .then(salt => bcrypt.hash(password, salt))
+      .then(hash => User.create({
+        name,
+        email,
+        password: hash
+      }))
+      .then(() => res.redirect('/'))
+      .catch(err => console.log(err))
+  })
 })
 
 router.get('/logout', (req, res) => {
